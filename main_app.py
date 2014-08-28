c636a234ebc4eb683d4f02a5f4801e618538802f
Christopher De Vries <devries@idolstarastronomer.com>
2014-08-28 14:45:28 -0700

    Forgot to import os

--- main_app.py
+++ main_app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import bottle
 import subprocess
+import os
 
 p1 = subprocess.Popen(['ip','addr','show','eth0'],stdout=subprocess.PIPE)
 p2 = subprocess.Popen(['sed','-rn',r's/\s*inet\s(([0-9]{1,3}\.){3}[0-9]{1,3}).*/\1/p'],stdin=p1.stdout,stdout=subprocess.PIPE)
