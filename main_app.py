a5ffafa7c96e71209664d85433dc769da1bde591
Christopher De Vries <devries@idolstarastronomer.com>
2014-08-28 14:01:56 -0700

    Added a JSON endpoint

--- main_app.py
+++ main_app.py
@@ -14,6 +14,16 @@ app = bottle.app()
 def root_index():
     return bottle.template('index',ip_addr = ip_addr)
 
+@bottle.route('/json')
+def json_reply():
+    heads = bottle.request.headers
+    bottle.response.content_type = 'application/json'
+
+    response = {'headers':dict(heads),
+            'environment':dict(os.environ),
+            'response':dict(bottle.response.headers)}
+    return response
+    
 if __name__=='__main__':
     bottle.debug(True)
     bottle.run(app=app,host='localhost',port=8080)
